620edde41c700e5a974441cb71f7e6242686734f
triz193 <112577389+triz193@users.noreply.github.com>
2024-03-05 15:40:21 -0300

    Add files via upload

---
+++ src/Tile.lua
@@ -0,0 +1,61 @@
+--[[
+    GD50
+    Match-3 Remake
+
+    -- Tile Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    The individual tiles that make up our game board. Each Tile can have a
+    color and a variety, with the varietes adding extra points to the matches.
+]]
+
+Tile = Class{}
+
+function Tile:init(x, y, color, variety, shiny)
+    
+    -- board positions
+    self.gridX = x
+    self.gridY = y
+
+    -- coordinate positions
+    self.x = (self.gridX - 1) * 32
+    self.y = (self.gridY - 1) * 32
+
+    -- tile appearance/points
+    self.color = color
+    self.variety = variety
+
+    -- shiny flag
+    self.shiny = shiny or false
+    
+end
+
+function Tile:render(x, y)
+    
+    -- draw shadow
+    love.graphics.setColor(34, 32, 52, 255)
+    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
+        self.x + x + 2, self.y + y + 2)
+
+    -- draw tile itself
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
+        self.x + x, self.y + y)
+
+    -- draw shiny effect
+    if self.shiny then
+        love.graphics.setColor(255, 255, 255, 255) -- White color
+        love.graphics.setLineWidth(2) -- Line width for the rounded rectangle
+
+        local radius = 4 -- Radius for the rounded corners
+        local width = 24 -- Width of the rectangle
+        local height = 24 -- Height of the rectangle
+
+        love.graphics.rectangle('line', self.x + x + 4, self.y + y + 4, width, height, radius)
+        
+    end
+
+end
+
